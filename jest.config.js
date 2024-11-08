cbfa4f8d07db716c701d249fb667f13562e5e6bc
Максим Мальцев <maltcevms@yandex.ru>
2024-11-08 19:54:26 +0500

    instal jsdom

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transform: {
     '\\.js': 'babel-jest',
   },
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '\\.(css|less|scss)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|webp|svg)$': 'jest-transform-stub',
